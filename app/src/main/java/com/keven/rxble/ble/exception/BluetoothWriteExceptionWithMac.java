5acb61ef54e81f1c32d118d61c24cf77a803499f
Keven_Company <tv2014@126.com>
2019-05-22 13:52:55 +0800

    升级了gradle

---
+++ app/src/main/java/com/keven/rxble/ble/exception/BluetoothWriteExceptionWithMac.java
@@ -0,0 +1,11 @@
+package com.keven.rxble.ble.exception;
+
+/**
+ * Created by Billin on 2017/4/14.
+ */
+public class BluetoothWriteExceptionWithMac extends BluetoothExceptionWithMac {
+
+    public BluetoothWriteExceptionWithMac(String msg, String mac) {
+        super(msg, mac);
+    }
+}
